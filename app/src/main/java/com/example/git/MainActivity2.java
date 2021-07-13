b76cfde5658a709eda7e2bbcc0f4e36812913df1
4mit <transfarmer@gmail.com>
2021-07-13 22:50:20 +0530

    New Activity Created

---
+++ app/src/main/java/com/example/git/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.example.git;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        button = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity2.this, "This is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
