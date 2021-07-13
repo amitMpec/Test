ab03e79eea62983d83885661c61351f621e54a9c
4mit <transfarmer@gmail.com>
2021-07-13 19:00:59 +0530

    Initial Commit v1.0

---
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.git;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView; 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Image have been clicked..", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
