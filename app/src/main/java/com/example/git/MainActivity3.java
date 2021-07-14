fd442ef90f7db2c250690fbd4c7525b817cf7c6b
4mit <teluskotraining1@gmail.com>
2021-07-14 12:14:13 +0530

    Newamitbranch added

--- app/src/main/java/com/example/git/MainActivity3.java
+++ app/src/main/java/com/example/git/MainActivity3.java
@@ -18,5 +18,6 @@ public class MainActivity3 extends AppCompatActivity {
         Toast.makeText(this, "this is seventh branch", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "This is telusko training branch", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "This is Amit branch", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "New Amit", Toast.LENGTH_SHORT).show();
     }
 }
