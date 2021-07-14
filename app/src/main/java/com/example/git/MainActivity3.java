b9dd918d78211d768aff0108f968cbcabccc2dd6
4mit <itamit420@gmail.com>
2021-07-14 12:02:28 +0530

    seventhbranch added

--- app/src/main/java/com/example/git/MainActivity3.java
+++ app/src/main/java/com/example/git/MainActivity3.java
@@ -15,5 +15,6 @@ public class MainActivity3 extends AppCompatActivity {
         Toast.makeText(this, "Hi there", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "This is fifth branch", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "This is sixth branch", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "this is seventh branch", Toast.LENGTH_SHORT).show();
     }
 }
